a9ab3422dbb99e29fe2ef6ce17b2338bc4aef3a8
Anton <gh.termythter@gmail.com>
2024-11-01 13:35:51 +0200

    added endpoint get product by id

--- product.routes.js
+++ product.routes.js
@@ -9,6 +9,16 @@ router.get('/products', (request, response) => {
     return response.json(products);
 });
 
+// handle get request for path /products/:id
+router.get('/products/:id(\\d+)', (request, response) => {
+    const { id } = request.params; // Access the id parameter from the URL
+
+    // Filter products based on the id parameter
+    const filteredProducts = products.filter(product => product.id === +id);
+
+    response.json(filteredProducts); // Send the filtered products as a JSON response
+})
+
 // handle get request for path /products/:brand
 router.get('/products/:brand', blockSpecialBrand, (request, response) => {
     const { brand } = request.params; // Access the brand parameter from the URL
